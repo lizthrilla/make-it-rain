ec80ab15f75964bf49251ee7880916b7407aa63a
Liz Tiller <elizabeth.tiller@gmail.com>
2016-11-22 18:20:04 -0500

    one drop moving no button

--- public/main.js
+++ public/main.js
@@ -1,13 +1,26 @@
 const qs = s => document.querySelector(s)
 const qsa = s => document.querySelectorAll(s)
-const drop = qs('.droplet')
 
 const main = () => {
+  const drop = qs('.droplet')
 
+  drop.style.top = '0px'
 
+  setInterval(() => {
+    drop.style.top = '0px'
+  }, 6000)
 
+  const animate = () => {
+    drop.style.top = `${parseInt(drop.style.top) + 1}px`
+  }
 
-  //document.getElementById('root').textContent = 'Hello, World!'
+  setInterval(animate, 12)
+
+  /*setTimeout(() => {
+   drop.style.backgroundColor = 'green'
+ }, 5000)*/
 }
+  //document.getElementById('root').textContent = 'Hello, World!'
+
 
 document.addEventListener('DOMContentLoaded', main)
