06b03b0b2a340c274c6d671c2fa479d57b1d5e49
Liz Tiller <elizabeth.tiller@gmail.com>
2016-11-22 17:45:47 -0500

    all the elements are in, just need to make them move

--- public/main.js
+++ public/main.js
@@ -1,6 +1,13 @@
+const qs = s => document.querySelector(s)
+const qsa = s => document.querySelectorAll(s)
+const drop = qs('.droplet')
+
 const main = () => {
-  document.getElementById('root').textContent = 'Hello, World!'
+
+
+
+
+  //document.getElementById('root').textContent = 'Hello, World!'
 }
 
 document.addEventListener('DOMContentLoaded', main)
-
