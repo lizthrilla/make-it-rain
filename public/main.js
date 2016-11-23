40c81589dc4022916ba37e6100423ad9e8dc12b8
Liz Tiller <elizabeth.tiller@gmail.com>
2016-11-23 00:48:44 -0500

    drop animating with button

--- public/main.js
+++ public/main.js
@@ -1,26 +1,26 @@
 const qs = s => document.querySelector(s)
 const qsa = s => document.querySelectorAll(s)
+const drop = qs('.droplet')
+  drop.style.visibility = "hidden"
 
 const main = () => {
-  const drop = qs('.droplet')
-
-  drop.style.top = '0px'
-
+    drop.style.visibility = "visible"
+    drop.style.top = "0px"
+    drop.style.left = "0px"
   setInterval(() => {
     drop.style.top = '0px'
   }, 6000)
-
   const animate = () => {
     drop.style.top = `${parseInt(drop.style.top) + 1}px`
   }
-
   setInterval(animate, 12)
-
+}
   /*setTimeout(() => {
    drop.style.backgroundColor = 'green'
- }, 5000)*/
-}
+ }, 5000)
+*/
+qs('button').addEventListener('click', main)
   //document.getElementById('root').textContent = 'Hello, World!'
 
-
-document.addEventListener('DOMContentLoaded', main)
+//document.addEventListner('click', main)
+//document.addEventListener('DOMContentLoaded', main)
