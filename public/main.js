bac50340263119ce73fddd93deb6f7319aef4b05
Liz Tiller <elizabeth.tiller@gmail.com>
2016-11-23 19:22:32 -0500

    appendChild put in

--- public/main.js
+++ public/main.js
@@ -1,20 +1,34 @@
 const qs = s => document.querySelector(s)
 const qsa = s => document.querySelectorAll(s)
+
+
 const drop = qs('.droplet')
   drop.style.visibility = "hidden"
 
-const main = () => {
-    drop.style.visibility = "visible"
-    drop.style.top = "0px"
-    drop.style.left = "Math.round(Math.random) + px"
-  setInterval(() => {
-    drop.style.top = '0px'
-  }, 6000)
-  const animate = () => {
-    drop.style.top = `${parseInt(drop.style.top) + 1}px`
-  }
-  setInterval(animate, 12)
+const newdrop = () => {
+  const li = document.createElement('li')
+  const figure = document.createElement('drop')
+  figure.style = drop
+  li.appendChild(drop)
+  qs('ul').appendChild(li)
 }
+
+
+const main = () => {
+  newdrop()
+}
+  //drop.style.visibility = "visible"
+  //drop.style.top = "0px"
+
+  //setInterval(() => {
+    //drop.style.top = '0px'
+  //}, 6000)
+
+  //const animate = () => {
+    //drop.style.top = `${parseInt(drop.style.top) + 1}px`
+  //}
+  //setInterval(animate, 12)
+//}
   /*setTimeout(() => {
    drop.style.backgroundColor = 'green'
  }, 5000)
