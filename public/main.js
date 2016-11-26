0b2469268536e051803b1cf4e262c31203384120
Liz Tiller <elizabeth.tiller@gmail.com>
2016-11-26 13:17:03 -0500

    most the parts working but not together

--- public/main.js
+++ public/main.js
@@ -1,40 +1,64 @@
+/*CreateDrop function:
+  Create a new drop
+  Append it to the drops container
+
+Animate function:
+  For every drop in the drops container,
+    Increase it's 'top' property
+
+Main function:
+  When a button is clicked, call CreateDrop function
+  At an interval, call Animate function
+
+When document is ready, call Main function*/
+
 const qs = s => document.querySelector(s)
 const qsa = s => document.querySelectorAll(s)
 
 
-const drop = qs('.droplet')
-  drop.style.visibility = "hidden"
+ const drop = qs('.droplet')
+ drop.setAttribute('height', '40px')
+ drop.setAttribute('width', '20px')
 
-const newdrop = () => {
-  const li = document.createElement('li')
-  const figure = document.createElement('drop')
-  figure.style = drop
-  li.appendChild(drop)
-  qs('ul').appendChild(li)
+const createDrop = () => {
+  const rain = document.createElement('drop')
+  //qs('figure').appendChild(rain)
+  document.getElementById('container').appendChild(rain)
+  rain.style.visibility ="visible"
+  drop.style.top = '0px'
+  console.log("drip drop")
+}
+const randomNumber = () => {
+  Math.floor((Math.random() * 100))
 }
 
+const animate = () => {
+  drop.style.top = `${parseInt(drop.style.top) + 1}px`
+  drop.style.left = `${randomNumber()}px`
+}
+setInterval(animate, 9)
 
 const main = () => {
-  newdrop()
+  //qs('button').addEventListener('click',)
+  createDrop()
 }
-  //drop.style.visibility = "visible"
-  //drop.style.top = "0px"
+  /*drop.style.visibility = "visible"
+  drop.style.top = "0px"
 
-  //setInterval(() => {
-    //drop.style.top = '0px'
-  //}, 6000)
+  setInterval(() => {
+    drop.style.top = '0px'
+  }, 6000)
 
-  //const animate = () => {
-    //drop.style.top = `${parseInt(drop.style.top) + 1}px`
-  //}
-  //setInterval(animate, 12)
-//}
+  const animate = () => {
+    drop.style.top = `${parseInt(drop.style.top) + 1}px`
+  }
+  setInterval(animate, 12)
+}
   /*setTimeout(() => {
    drop.style.backgroundColor = 'green'
  }, 5000)
 */
 qs('button').addEventListener('click', main)
-  //document.getElementById('root').textContent = 'Hello, World!'
 
 //document.addEventListner('click', main)
 //document.addEventListener('DOMContentLoaded', main)
